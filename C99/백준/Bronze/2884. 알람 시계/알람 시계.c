eadc3d3c3a2fe262df7a63d899c1fbc8c03e41a9
ericlair <163988535+ericlair@users.noreply.github.com>
2026-03-24 19:22:08 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C99/백준/Bronze/2884. 알람 시계/알람 시계.c
+++ C99/백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-int main(void){
-    int H, M;
+int main(void){ 
 
-    scanf("%d %d", &H, &M);
-    int A = H, B = M;
-    if (M - 45 < 0){
-        if (H == 0){
-            A = 23;
-        } else{
-            A = A - 1;
-        } B = M + 15;
-    } else{
-        B = M - 45;
-    }
-    printf("%d %d", A, B);
+    int H, M;
     
+    scanf("%d %d", &H, &M);
+
+    int T = (H*60) + M;
+
+    if (T == 0) printf("23 15");
+    else if(T < 45) printf("23 %d", T + 15);
+    else if(T%60 <45) printf("%d %d",T/60 - 1, T%60 + 15);
+    else printf("%d %d", T/60, T%60 - 45);
+
+    return 0;
+
 }
