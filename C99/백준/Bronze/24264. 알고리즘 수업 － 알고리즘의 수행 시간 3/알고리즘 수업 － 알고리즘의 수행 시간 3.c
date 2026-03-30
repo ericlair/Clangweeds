1228f2099144a00da6e9e3dd87824ca8478a6b57
ericlair <163988535+ericlair@users.noreply.github.com>
2026-03-30 16:57:26 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    long long n;
+    scanf("%lld", &n);
+
+    long long count = n * n;
+    printf("%lld\n", count);
+
+    int degree = 2;
+    printf("%d\n", degree);
+
+    return 0;
+}
+
