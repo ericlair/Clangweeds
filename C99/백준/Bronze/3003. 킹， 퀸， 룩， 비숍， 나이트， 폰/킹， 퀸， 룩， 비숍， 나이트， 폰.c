b1823b308803b7be1904003df84cff38acfe8805
ericlair <163988535+ericlair@users.noreply.github.com>
2026-04-01 16:12:42 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C99/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
+++ C99/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -1,46 +1,17 @@
 #include <stdio.h>
 
 int main(void){
+    int chess[6] = {1, 1, 2, 2, 2, 8};
+    int input[6] = {0, };
 
-    int king, queen, bishop, rook, knight, pawn;
-    int a, b, c;
-
-    scanf("%d %d %d %d %d %d", &king, &queen, &bishop, &rook, &knight, &pawn);
-    
-    for (int i = 1; i > -10 ; i--){
-        if (king + i == 1){
-            a = i;
-        }
-        if (queen + i == 1){
-            b = i;
-        }
+    for (int i = 0; i < 6; i++){
+        scanf("%d", &input[i]);
     }
-    printf("%d %d ", a, b);    
-        
 
-
-    for (int i = 2; i >= -8 ; i--){
-        if (bishop + i == 2){
-            a = i;
-        }
-        if (rook + i == 2){
-            b = i;
-        }
-        if (knight + i == 2){
-            c = i;
-        }
-    }
-        
-    printf("%d %d %d ", a, b, c);   
-
-    
-    for (int i = 8; i >= -2 ; i--){
-        if (pawn + i == 8){
-            a = i;
-        }
+    for (int i = 0; i < 6; i++){
+        printf("%d ",chess[i] - input[i]);
     }
     
-    printf("%d", a);  
-    
-
+    return 0;
 }
+
