4f94df0171ae327c5fce6f2b813c9dd67d505d73
ericlair <163988535+ericlair@users.noreply.github.com>
2026-04-03 23:05:44 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++){
+        for (int k = n-i-1; k >= 0; k--){
+            printf(" ");
+        }       
+        for (int j = 1; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
