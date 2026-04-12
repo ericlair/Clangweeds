a31fab43e0520f1afd0bd981882574e04ede7d29
ericlair <163988535+ericlair@users.noreply.github.com>
2026-04-12 15:20:39 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C99/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
+++ C99/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
@@ -1,48 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
-    int Alpha[26];
-    char word[101];
-
-    for(int i = 0; i <= 25; i++){
-        Alpha[i] = i+97; 
-    }
-
-    scanf("%s", word);
-
-    int len = 0;
-    while (word[len] != 0){
-        len++;
-    }
-
-    for(int j = 0; j <= len; j++){
-        for (int i = 0; i <= 25; i++){
-            if (Alpha[i] == word[j]){
-                Alpha[i] = -2;
-            }
-        }
-    }
     
-    for (int i = 0; i <= 25; i++){
-        if (Alpha[i] != -2){
-            Alpha[i] = -1;
+    int Alpha[26] = {0, };
+    char S[101] = {0, };
+    int asc = 0;
+    
+    scanf("%s", S);
+
+    for (int i = 0; i <= 25; i++) {
+        Alpha[i] = -1;
+    }
+
+    for (int i = 0; i < strlen(S); i++) {
+        asc = S[i] - 'a';
+        if (Alpha[asc] == -1) {      
+            Alpha[asc] = i;
         }
     }
 
-    for(int j = 0; j <= len; j++){
-        for (int i = 0; i <= 25; i++){
-            if (word[j] - 97 == i && Alpha[i] == -2){
-                Alpha[i] = j;
-            }
-        }
-    }
-
-    for (int i = 0; i <= 25; i++){
+    for (int i = 0; i <= 25; i++) {
         printf("%d ", Alpha[i]);
     }
 
     return 0;
-
+    
 }
 
 
